ecb4e72e9a3ebbb8c51822eb02172db31631c888
Atul Rawat <atulrawat104903@gmail.com>
2026-09-20 22:01:14 +0530

    Write test for analyzeArray function

---
+++ analyzeArray.js
@@ -0,0 +1,10 @@
+const analyzeArray = (array) => {
+  const sum = array.reduce((total, number) => total + number, 0);
+  return {
+    average: sum / array.length,
+    min: Math.min(...array),
+    max: Math.max(...array),
+    length: array.length,
+  };
+};
+export default analyzeArray;
